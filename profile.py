afa55ebb26ba8f107dd4bc38c6a103df8795af21
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-10-09 20:50:04 -0600

    Trying to fix issue with interface creation

--- profile.py
+++ profile.py
@@ -24,17 +24,17 @@ request = pc.makeRequestRSpec()
 IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
 
 nodes = []
-link = request.LAN("lan")
+lan = request.LAN()
+lan.bandwidth = 10000000
 
 # Create controller node
 node = request.RawPC("GCM")
 node.disk_image = IMAGE
 node.hardware_type = params.nodeType
 iface = node.addInterface("if1")
-iface.component_id = "eth1"
 iface.addAddress(rspec.IPv4Address("192.168.6.10", "255.255.255.0"))
+land.addInterface(iface)
 nodes.append(node)
-link.addInterface(iface)
 
 # Create 3 worker nodes
 for i in range(1,4):
@@ -42,9 +42,8 @@ for i in range(1,4):
   node.disk_image = IMAGE
   node.hardware_type = params.nodeType
   iface = node.addInterface("if1")
-  iface.component_id = "eth1"
   iface.addAddress(rspec.IPv4Address("192.168.6." + str(10 - i), "255.255.255.0"))
-  nodes.append(node)
   link.addInterface(iface)
+  nodes.append(node)
 
 pc.printRequestRSpec()
