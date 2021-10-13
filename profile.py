29b32813f7216061d03a2bff84e17e786d06aa44
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-10-13 13:13:22 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,6 +10,13 @@ import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as rspec
 
+# Profile Configuration Constants
+IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
+STORAGE = "10GB"
+# Based on how IPs are created below, NUM_WORKERS must be < 10
+NUM_WORKERS = 3
+BANDWIDTH = 10000000
+
 # Set up parameters
 pc = portal.Context()
 pc.defineParameter("nodeType", 
@@ -21,29 +28,38 @@ params = pc.bindParameters()
 pc.verifyParameters()
 request = pc.makeRequestRSpec()
 
-IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
-
+# Initial setup
 nodes = []
 lan = request.LAN()
-lan.bandwidth = 10000000
+lan.bandwidth = BANDWIDTH
 
 # Create controller node
 node = request.RawPC("GCM")
 node.disk_image = IMAGE
 node.hardware_type = params.nodeType
+nodes.append(node)
+
+# Add controller interface
 iface = node.addInterface("if1")
 iface.addAddress(rspec.IPv4Address("192.168.6.10", "255.255.255.0"))
 lan.addInterface(iface)
-nodes.append(node)
 
 # Create 3 worker nodes
-for i in range(1,4):
+for i in range(1,NUM_WORKERS + 1):
+  # Create node
   node = request.RawPC("node-" + str(i))
   node.disk_image = IMAGE
   node.hardware_type = params.nodeType
+  nodes.append(node)
+  
+  # Add interface
   iface = node.addInterface("if1")
   iface.addAddress(rspec.IPv4Address("192.168.6." + str(10 - i), "255.255.255.0"))
   lan.addInterface(iface)
-  nodes.append(node)
+  
+  # Add extra storage space
+  bs = node.Blockstore(name + "-bs", "/mydata")
+  bs.size = STORAGE
+  bs.placement = "any"
 
 pc.printRequestRSpec()
