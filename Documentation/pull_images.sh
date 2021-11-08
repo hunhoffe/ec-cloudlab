60f3676f264d73458b4fef04158c8c3361b70f3a
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-11-08 07:23:03 -0700

    Create pull_images.sh

---
+++ Documentation/pull_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IMAGE_DIR=/home/ec/k8s-images
+
+# Create persistent directory for images
+sudo mkdir $IMAGE_DIR
+sudo chgrp ecuser $IMAGE_DIR
+sudo chmod -R g+rw $IMAGE_DIR
+
+# Iterate through necessary kubeadm images
+sudo kubeadm config images list | while read line
+do
+       # Pull the image, this will save in /mydata/docker -> but this isn't image persistent
+       sudo docker pull $line
+
+       # Remove image repo prefix (which messes with file creation) and leave just image name & version
+       echo $line
+       IMAGE_NAME="$(cut -d'/' -f2 <<< "$line")"
+       echo IMAGE_NAME
+
+       # Save the image locally in $IMAGE_DIR, since this is image persistent. 
+       sudo docker save $line > $IMAGE_DIR/$IMAGE_NAME.tar
+done
