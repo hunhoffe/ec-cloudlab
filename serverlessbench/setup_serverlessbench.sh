8eda515024f1cafd0443956b2d1ebb344dd15369
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-01-10 15:14:40 -0700

    Create couchdb database for images

--- serverlessbench/setup_serverlessbench.sh
+++ serverlessbench/setup_serverlessbench.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Configuration Values
+COUCHDB_USERNAME=admin
+COUCHDB_PASSWORD=password
+COUCHDB_PORT=5984
+IMAGE_DATABASE=image_database
+
 # Set wsk properties
 wsk property set --apihost 192.168.6.1:31001
 wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
@@ -31,13 +37,17 @@ cd ~
 kubectl create secret generic my-db-couchdb --from-literal=adminUsername=admin --from-literal=adminPassword=password --from-literal=cookieAuthSecret=secret
 helm repo add couchdb https://apache.github.io/couchdb-helm
 helm install my-db --set createAdminSecret=false --set couchdbConfig.couchdb.uuid=decafbaddecafbaddecafbaddecafbad couchdb/couchdb
-sleep 30
 
-kubectl get services | grep 
+# Wait for it to deploy
+sleep 90
 
-# Set environment variables in bashrc
-echo "export COUCHDB_USERNAME=admin" | sudo tee -a ~/.bashrc
-echo "export COUCHDB_PASSWORD=password" | sudo tee -a ~/.bashrc
+# Set environment variables in bashrc for couchdb config
+echo "export COUCHDB_USERNAME=$COUCHDB_USERNAME" | sudo tee -a ~/.bashrc
+echo "export COUCHDB_PASSWORD=$COUCHDB_PASSWORD" | sudo tee -a ~/.bashrc
 COUCHDB_IP=$(kubectl get services | grep "svc-couchdb" | awk '{print $3}')
 echo "export COUCHDB_IP=$COUCHDB_IP" | sudo tee -a ~/.bashrc
-echo "export COUCHDB_PORT=5984" | sudo tee -a ~/.bashrc
+echo "export COUCHDB_PORT=$COUCHDB_PORT" | sudo tee -a ~/.bashrc
+
+# Create an image database and set environment variable in bashrc
+curl -X PUT http://$COUCHDB_USERNAME:$COUCHDB_PASSWORD@$COUCHDB_IP:$COUCHDB_PORT/$IMAGE_DATABASE
+echo "export IMAGE_DATABASE=$IMAGE_DATABASE" | sudo tee -a ~/.bashrc
