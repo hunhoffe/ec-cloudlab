c1f0767be3977cac8d34ff27ee84263fed8b4a86
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-01-13 11:37:23 -0700

    Update setup_serverlessbench.sh

--- serverlessbench/setup_serverlessbench.sh
+++ serverlessbench/setup_serverlessbench.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+INSTALL_DIR="~"
+TEST_PATH="$INSTALL_DIR/ServerlessBench/Testcase4-Application-breakdown"
+CONFIG_FILE="local.env"
+CONFIG_FILE_PATH="$INSTALL_DIR/$CONFIG_FILE"
+COUCHDB_CONFIG="/local/repository/serverlessbench/couchdb-config.yaml"
+
 # Set wsk properties
 wsk property set --apihost 192.168.6.1:31001
 wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+
+# Necessary to run docker without sudo
+CURRENT_USER=$(whoami)
+sudo gpasswd -a $CURRENT_USER docker
 
 # Install serverlessbench dependencies
 # From: https://github.com/SJTU-IPADS/ServerlessBench/tree/master/Testcase4-Application-breakdown
@@ -13,24 +23,25 @@ sudo apt-get install -y maven nodejs jq\
                      libhiredis-dev texinfo automake libtool pkg-config python3-boto3
 
 # Clone the serverlessbench repo
-cd ~
+cd $INSTALL_DIR
 git clone https://github.com/hunhoffe/ServerlessBench.git
 
-# Necessary to run docker without sudo
-CURRENT_USER=$(whoami)
-sudo gpasswd -a $CURRENT_USER docker
+# Create local config file
+cp local.env $CONFIG_FILE
 
-# Set directory location
+# Set test location to config
 # From: https://github.com/SJTU-IPADS/ServerlessBench/tree/master/Testcase4-Application-breakdown
-export TESTCASE4_HOME=~/ServerlessBench/Testcase4-Application-breakdown
-echo 'export TESTCASE4_HOME=~/ServerlessBench/Testcase4-Application-breakdown' | sudo tee -a ~/.bashrc
+echo 'TESTCASE4_HOME=$TEST_PATH' | sudo tee -a $CONFIG_FILE
+
+# Get COUCHDB_* values from config file
+source $CONFIG_FILE
 
 # Create couchdb deployment
 # Instructions from: https://artifacthub.io/packages/helm/couchdb/couchdb#configuration
-cd ~
-kubectl create secret generic my-db-couchdb --from-literal=adminUsername=admin --from-literal=adminPassword=password --from-literal=cookieAuthSecret=secret
+cd $INSTALL_DIR
+kubectl create secret generic my-db-couchdb --from-literal=adminUsername=$COUCHDB_USERNAME --from-literal=adminPassword=$COUCHDB_PASSWORD --from-literal=cookieAuthSecret=secret
 helm repo add couchdb https://apache.github.io/couchdb-helm
-helm install my-db --set createAdminSecret=false --set couchdbConfig.couchdb.uuid=decafbaddecafbaddecafbaddecafbad couchdb/couchdb -f /local/repository/serverlessbench/couchdb-config.yaml
+helm install my-db --set createAdminSecret=false --set couchdbConfig.couchdb.uuid=decafbaddecafbaddecafbaddecafbad couchdb/couchdb -f $COUCHDB_CONFIG
 
 # Wait for it to deploy
 sleep 90
@@ -44,8 +55,9 @@ echo $COUCHDB_PASSWORD | kubectl exec --namespace default -it my-db-couchdb-0 -c
     -d '{"action": "finish_cluster"}' \
     -u admin 
 
+# Set COUCHDB_IP address in config file
 COUCHDB_IP=$(kubectl get services | grep "svc-couchdb" | awk '{print $3}')
-echo "COUCHDB_IP=$COUCHDB_IP" | sudo tee -a $TESTCASE4_HOME/local.env
+echo "COUCHDB_IP=$COUCHDB_IP" | sudo tee -a $CONFIG_FILE
 
 # Create an image database and set environment variable in bashrc
 curl -X PUT http://$COUCHDB_USERNAME:$COUCHDB_PASSWORD@$COUCHDB_IP:$COUCHDB_PORT/$IMAGE_DATABASE
