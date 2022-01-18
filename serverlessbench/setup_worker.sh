67ec0106f239284e586283ffd0f0c7fcf7df0f3c
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-01-18 07:35:09 -0700

    Update setup_worker.sh

--- serverlessbench/setup_worker.sh
+++ serverlessbench/setup_worker.sh
@@ -4,4 +4,4 @@ docker login
 
 # Pull images needed for image-process benchmark
 sudo docker image pull openwhisk/java8action
-sudo docker image pull dplsming/java8action-imagemagic-profile
+sudo docker image pull hunhoffe/java8action-imagemagic
